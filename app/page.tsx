443dfaeccd440020a33ec8f79f481b94b8ab3c1a
port <schoolreef2@protonmail.com>
2023-06-25 13:50:48 +0300

    Initial

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+function Page() {
+  return (
+    <div>
+      <ConnectButton />
+      <h1 className="text-3xl">asdasdasdasdasdasd</h1>
+    </div>
+  );
+}
+
+export default Page;
